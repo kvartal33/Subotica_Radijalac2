c6bfd805696477866c08615e7854d77fca401d22
KVARTAL33 <info@kvartal33.rs>
2025-05-06 15:15:25 +0200

    Commit

---
+++ data.js
@@ -0,0 +1,281 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-spavaca-soba",
+      "name": "Spavaca soba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7120016023871507,
+          "pitch": 0.09444708685528802,
+          "rotation": 0,
+          "target": "2-hodnik-1"
+        },
+        {
+          "yaw": -0.41634303338235057,
+          "pitch": 0.18162123399132568,
+          "rotation": 0,
+          "target": "1-spavaca-soba-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-spavaca-soba-2",
+      "name": "Spavaca soba 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.015715907667067,
+          "pitch": 0.2976071797984474,
+          "rotation": 0,
+          "target": "0-spavaca-soba"
+        },
+        {
+          "yaw": 2.2819346960738454,
+          "pitch": 0.2326244434081559,
+          "rotation": 0,
+          "target": "2-hodnik-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hodnik-1",
+      "name": "Hodnik 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.363738357341676,
+          "pitch": 0.1794317666526304,
+          "rotation": 0,
+          "target": "5-kuhinja"
+        },
+        {
+          "yaw": -3.0448152752763455,
+          "pitch": 0.04954628511269732,
+          "rotation": 0,
+          "target": "0-spavaca-soba"
+        },
+        {
+          "yaw": 0.05438853402896626,
+          "pitch": 0.18789179087376695,
+          "rotation": 0,
+          "target": "3-hodnik-2"
+        },
+        {
+          "yaw": 1.9179528776110368,
+          "pitch": 0.0005034709164810636,
+          "rotation": 0,
+          "target": "4-kupatilo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hodnik-2",
+      "name": "Hodnik 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.01579156578828389,
+          "pitch": 0.3659968113903034,
+          "rotation": 0,
+          "target": "2-hodnik-1"
+        },
+        {
+          "yaw": 0.3973057179047643,
+          "pitch": -0.006023881614940052,
+          "rotation": 0,
+          "target": "5-kuhinja"
+        },
+        {
+          "yaw": -0.2865026372388453,
+          "pitch": 0.05128230886014329,
+          "rotation": 0,
+          "target": "4-kupatilo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kupatilo",
+      "name": "Kupatilo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.120564759030648,
+          "pitch": 0.1095998528248483,
+          "rotation": 0,
+          "target": "2-hodnik-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-kuhinja",
+      "name": "Kuhinja",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.461629548898877,
+          "pitch": 0.09845018053524335,
+          "rotation": 0,
+          "target": "2-hodnik-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
